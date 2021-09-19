34bbf1f5e30c604fb88e81f9be2454452d42f1e6
Anna Konovalova <konon.ana@yandex.ru>
2021-09-19 08:13:40 +0100

    feat:script added

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,35 @@
-const popup = document.querySelector('.popup')
-const popupOpenBtn = document.querySelector('.profile__edit-button');
-const popupCloseBtn = popup.querySelector('.popup__close');
+let popup = document.querySelector('.popup')
+
+let popupCloseBtn = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__item_name');
+let jobInput = popup.querySelector('.popup__item_job');
+
+/**/
+let popupOpenBtn = document.querySelector('.profile__edit-button');
+let profile_name = document.querySelector('.profile__info-name')
+let profile_job = document.querySelector('.profile__info-occupation')
+
 
 function popupToggle() {
-    popup.classList.toggle('popup__open')
-
+    popup.classList.toggle('popup_opened');
+    if (popup.classList.contains('popup_opened')) {
+        nameInput.value = profile_name.textContent
+        jobInput.value = profile_job.textContent
+    } else {
+        nameInput.value = '';
+        jobInput.value = '';
+    }
 }
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault()
+    profile_name.textContent = nameInput.value
+    profile_job.textContent = jobInput.value
+    popupToggle()
+}
+
 popupOpenBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
+formElement.addEventListener('submit', formSubmitHandler);
