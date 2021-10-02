e84943b9c8ffcb9d7de321545583dceffe411b93
Anna Konovalova <konon.ana@yandex.ru>
2021-10-02 18:26:50 +0100

    scripts new

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,63 @@ function formSubmitHandler(evt) {
 popupOpenBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler);
+
+/**/
+let popupOpenCard = document.querySelector('.profile__add-button');
+let cardNameInput = popup.querySelector('.popup__item_field_card-name');
+let cardLinkInput = popup.querySelector('.popup__item_field_card-link');
+
+function popupCardToggle() {
+    popup.classList.toggle('popup_opened');
+}
+popupOpenCard.addEventListener('click', popupCardToggle)
+popupCloseBtn.addEventListener('click', popupToggle);
+
+
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Новокузнецк',
+        link: './images/Верхоянск.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+const cardsList = document.querySelector('.elements__list');
+
+/**/
+
+const addCard = function(item) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.element__title').textContent = item.name;
+    cardElement.querySelector('.element__image').src = item.link;
+    cardElement.querySelector('.element__like-button').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('element__like-button_active');
+    });
+
+    cardElement.querySelector('.element__delete-button').addEventListener('click', function(evt) {
+        evt.target.parentElement.remove();
+    });
+    cardsList.append(cardElement);
+}
+initialCards.forEach(addCard);
